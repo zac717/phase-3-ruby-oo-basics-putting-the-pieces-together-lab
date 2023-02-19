576a09fa9232b22e536424ba43c950ddb2f9dd8c
Isaac Ongera <isaacongera717@gmail.com>
2023-02-19 10:07:32 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
 
 
+class Book
+    attr_reader :title
+    attr_accessor :page_count, :genre, :author
+
+    def initialize(title)
+        @title = title
+    end
+
+   
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+
+end
